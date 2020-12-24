5b7d9a65d9c4203f87261c41752d0aef2d998811
Sayali45 <51472379+Sayali45@users.noreply.github.com>
2020-12-24 13:10:45 +0530

    Commit addition file

---
+++ Addition/src/AddTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AddTest 
+{
+
+	@Test
+	public void Test() {
+	
+		assertEquals( "error in Add()", 4, Add.add(1,4));
+	}
+
+}
